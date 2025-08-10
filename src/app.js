8cbfdfc1b10401af1de0ad68576bf5b4c4ceb2ad
mokwa the developer <moffatmokwa12@gmail.com>
2025-08-10 14:42:31 +0300

    feat: Register product routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const connectDB = require('./config/database');
 const healthRouter = require('./routes/health');
 const authRouter = require('./routes/auth');
+const productRouter = require('./routes/product');
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.use(express.json());
 // Routes
 app.use('/health', healthRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/products', productRouter);
 
 module.exports = app;
