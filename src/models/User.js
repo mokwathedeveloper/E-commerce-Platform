081988d008c20b7f05956bd7be474d778b98f17d
mokwa the developer <moffatmokwa12@gmail.com>
2025-08-10 14:47:53 +0300

    feat: Add cart field to User model

--- src/models/User.js
+++ src/models/User.js
@@ -4,6 +4,12 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  cart: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+      quantity: { type: Number, default: 1 },
+    },
+  ],
 });
 
 module.exports = mongoose.model('User', userSchema);
